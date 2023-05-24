f72db00712c5c9fb6fa688433135752cdf18f4ab
berenbarlage <berenbarlage@gmail.com>
2023-05-24 16:11:03 +0200

    dit is de eerste versie van de code die berichten en fotos ontvangt.

--- vogel AI.py
+++ vogel AI.py
@@ -1,41 +1,33 @@
 import discord
 from discord.ext import commands
 
+TOKEN = 'MTA5NTczMDQ3ODMyNzI3OTcyMA.GrwORH.sDOXylbznFtkFS4WynAffbYR5knUHeiyYHg5DQ'
+
 intents = discord.Intents.default()
 intents.typing = False
-intents.presences = False
+
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-# opstarten van de bot
 @bot.event
 async def on_ready():
-    print('Bot is ingelogd als {0.user}'.format(bot))
+    print('Bot is klaar')
 
-# ontvangen van berichten
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    # Bericht
-    if message.content:
-        await message.channel.send(f'Je hebt een bericht gestuurd ')
+    print(message.author, message.content, message.channel.id)
 
-    # Foto's
-    if len(message.attachments) > 0:
+    if message.attachments:
         for attachment in message.attachments:
             await attachment.save(attachment.filename)
-            await message.channel.send(f'Je hebt een foto gestuurd')
+            print(f'Ontvangen bijlage: {attachment.filename}')
 
     await bot.process_commands(message)
 
 
-
-
-bot.run('MTA5NTczMDQ3ODMyNzI3OTcyMA.GrwORH.sDOXylbznFtkFS4WynAffbYR5knUHeiyYHg5DQ')
-
-
-
+bot.run(TOKEN)
 
